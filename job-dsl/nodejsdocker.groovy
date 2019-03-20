b700a22a2144311ca73eec966e0291ffb63e733f
prabhawm <30548157+prabhawm@users.noreply.github.com>
2019-03-20 15:37:04 +0800

    update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('prabhawm/docker-nodejs-demo')
+            repositoryName('prabhawm/docker-nodejs-app')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
